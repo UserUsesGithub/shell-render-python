78d676fe6afc5d55b8cc7cf68d9cd9d57d1d1394
UserUsesGithub <127356804+UserUsesGithub@users.noreply.github.com>
2023-04-15 13:31:03 -0400

    added new function

--- rendering_code/main.py
+++ rendering_code/main.py
@@ -1,6 +1,8 @@
 import os
 from sys import platform
 import time
+
+
 def render(text,*args):
     #simple rendering by clearing the screen...
     if platform == 'win32':
@@ -22,6 +24,8 @@ def render(text,*args):
         pass
     if success:
         return outputval2
+
+
 def modify(text,new,x,y):
     #split it with y
     text2=text.split('\n')
@@ -35,6 +39,8 @@ def modify(text,new,x,y):
         text2[i[0]] = ''.join(i[1])
     final = '\n'.join(text2)
     return final
+
+
 def tell(text,x,y):
     #split the text so it can be used as a list
     text2=text.split('\n')
@@ -42,9 +48,20 @@ def tell(text,x,y):
     for i in enumerate(text2):
         text2[i[0]] = list(i[1])
     return text2[y][x]
+
+
 def all_y(text):
     #find all y values
     text2 = text.split('\n')
     for i in enumerate(text2):
         if i[1] != '':
             yield i[0],i[1]
+
+
+def raw_modify(text,new,loc):
+    text2 = list(text)
+    if text2[loc] == '\n':
+        raise Exception("Raw cannot be \\n")
+    text2[loc] = new
+    
+    return "".join(text2)
