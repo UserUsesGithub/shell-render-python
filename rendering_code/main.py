7e9f694079c1ce6a42dad961cd7a5ff0d2f003a6
UserUsesGithub <127356804+UserUsesGithub@users.noreply.github.com>
2023-04-15 13:21:28 -0400

    Added  comments to main
    nothing else

--- rendering_code/main.py
+++ rendering_code/main.py
@@ -2,11 +2,14 @@ import os
 from sys import platform
 import time
 def render(text,*args):
+    #simple rendering by clearing the screen...
     if platform == 'win32':
         os.system('cls')
     else:
         os.system('clear')
+        #and printing
     print(text)
+    #now here is a bunch of argument stuff
     success = False
     try:
         time.sleep(args[0])
@@ -20,20 +23,27 @@ def render(text,*args):
     if success:
         return outputval2
 def modify(text,new,x,y):
+    #split it with y
     text2=text.split('\n')
+    #add x
     for i in enumerate(text2):
         text2[i[0]] = list(i[1])
+    #replace it
     text2[y][x] = new
+    #join everything back together
     for i in enumerate(text2):
         text2[i[0]] = ''.join(i[1])
     final = '\n'.join(text2)
     return final
 def tell(text,x,y):
+    #split the text so it can be used as a list
     text2=text.split('\n')
+    #GO THROUGH IT TO FIND THE THING YOU ARE TRYING TO
     for i in enumerate(text2):
         text2[i[0]] = list(i[1])
     return text2[y][x]
 def all_y(text):
+    #find all y values
     text2 = text.split('\n')
     for i in enumerate(text2):
         if i[1] != '':
